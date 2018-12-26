6115436d9915cb77e8591f016c44c7754849a36b
zain00 <30941046+zain00@users.noreply.github.com>
2018-12-26 11:58:18 +0700

    Create user.php

---
+++ user.php
@@ -0,0 +1,30 @@
+<?php
+echo $red."[!] warning !!!".$t;
+echo $kuning."[-] ini adalah program ilegal".$t;
+echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
+$sungging = $putih.'[*] '.$ijo.'subcribe youtube sungging '.$putih.'[-] code: '.$ijo.'L41023';
+$ads= $putih."[÷]    official channel    [÷]".$t.$ijo."[-] adi bordir ".$putih."| atau |".$ijo." sungging [-]";
+$msgtoto = "[!] Subscribe Youtube adi bordir dan Sungging".$t.$red."[°] demi keamanan hentikan aktifitas bot sekarang !!!!".$t.$kuning."[•] ctrl + c";
+$msgyd = "[!] ganguan pada server jangan panik ini tidak lama";
+$yd_stat = true;
+$vip1= false;
+$weblock =[""];
+$ydblock = [""];
+$we_stat = true;
+$msgwe = "[!] enjoy your life";
+$msgveeu = "[!] enjoy your life";
+
+$data= [
+'Zainul Muhaimin','Aduh Sopo','Zain','Abdul Lathief','David Sanders',Abi Manyu,
+
+//vipm
+'','',
+//mpiv
+/*
+ 'N58140','O10784','J15232','V27584','M94778','V98855','H44906','U61395',
+ */
+
+];
+
+$toto = ['tes',];
+$ydnews = ['tes','adidoank',];
